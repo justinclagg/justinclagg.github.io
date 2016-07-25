abd4b4b653b59ba6f858a2f0a9d3b26ce24a8459
Justin Clagg <justinclagg@gmail.com>
2016-07-24 23:51:37 -0400

    portfolio update, added about and contact pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,17 @@
+
 $(document).ready(function() {
 	"use strict";
 
-	var activeNav = "#portfolio-nav";
-
+	// var activeNav = "#portfolio-nav";
 	// Change active page
-	$("#nav-links > ul> li > a").click(function() {
-		$(activeNav).removeClass("active-nav");
-		$(this).addClass("active-nav");
-		activeNav = "#" + $(this).attr("id");
-	});
-
-
-
-
-
-
+	// $("#nav-links > ul> li > a").click(function() {
+	// 	$(activeNav).removeClass("active-nav");
+	// 	$(this).addClass("active-nav");
+	// 	activeNav = "#" + $(this).attr("id");
+	// });
 
 	// Place current year in footer
-	var dateVal = new Date();
-	var yearVal = dateVal.getFullYear();
-	$("#current-year").text(yearVal);
+	// var dateVal = new Date();
+	// var yearVal = dateVal.getFullYear();
+	// $("#current-year").text(yearVal);
 });

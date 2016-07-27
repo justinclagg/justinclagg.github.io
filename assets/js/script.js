bd6ae2999131e8dd31fa4eedfd6a48645200dc89
Justin Clagg <justinclagg@gmail.com>
2016-07-26 20:54:02 -0400

    fixed contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,29 @@
 $(document).ready(function() {
 	"use strict";
 
-	$("#contact-form").submit(function(event) {
-		var message = $(this).serialize();
-		console.log(message);
+	$("#contact-form").submit(function() {
+
+		var name = document.getElementById("name-form"),
+			email = document.getElementById("email-form"),
+			message = document.getElementById("message-form");
+
 		$.ajax({
-			url: "https://formspree.io/justinclagg@gmail.com", 
+			url: "//formspree.io/justinclagg@gmail.com", 
 			method: "POST",
-			data: {message: message},
+			data: {
+				name: name.value,
+				_replyto: email.value,
+				message: message.value
+			},
 			dataType: "json"
+		}).done(function() {
+			$(".form-header").text("Your message has been sent!");
+			name.value = email.value = message.value = "";
+		}).fail(function() {
+			$(".form-header").text("Sorry, there was an error!");
 		});
-		$(".form-header").text("Your message has been sent!");
-		return false;
+
+		return false;  // Prevents page reload
 	});
+
 });
